886f0bf9dc1ca809122cd7a821c19a9d7c94cfd9
Murillo Alves <tumusx1@gmail.com>
2025-05-11 23:58:12 -0300

    modify index ts

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import { registerRootComponent } from 'expo';
 
-import App from './App';
+import App from './presenter/container/App';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
